694a9e89d1455a2544aabff3247ae3b599d11835
Ankit <ankitsinghal1321@gmail.com>
2026-09-05 21:02:59 +0530

    Add Book class with methods for borrowing and returning books

---
+++ challenge/OOP/Book.java
@@ -0,0 +1,22 @@
+public class Book{
+
+    String title, author, isbn;
+
+    static int totalBooks = 0;
+    
+    public void returnBook(){
+
+    }
+
+    
+    public void borrowBook(){
+
+    }
+
+    public int getTotalBooks(){
+
+        return totalBooks;
+    }
+
+
+}
